cf8ac8eeb9a0a69ac3f5efb0a52d8f15d8e6d706
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-25 14:16:52 +0200

    add api to get and create employee

---
+++ Backend/src/main/java/com/example/Backend_Springboot/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.example.Backend_Springboot.controller;
+
+import com.example.Backend_Springboot.model.Employee;
+import com.example.Backend_Springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //Get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    //Create employee
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+
+
+}
