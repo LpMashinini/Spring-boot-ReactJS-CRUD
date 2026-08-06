f8f8617254b82870d08683ae10c14bd3bf38f71a
LpMashinini <lebogangmatshinini@gmail.com>
2026-08-06 22:46:03 +0200

    add ListUserComponent and HeaderComponent

---
+++ Frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HeaderComponent = () => {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <div>
+                    <a href="#" className="navbar-brand">
+                        Employee Management App
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default HeaderComponent;
