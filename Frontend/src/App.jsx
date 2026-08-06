8c5da183183618de62abbee1e7140789b50a47f7
LpMashinini <lebogangmatshinini@gmail.com>
2026-08-06 23:19:46 +0200

    add react-router dependency and add App class code

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import ViewEmployeeComponent from './components/ViewEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<ListEmployeeComponent />} />
+            <Route path="/employees" element={<ListEmployeeComponent />} />
+            <Route path="/add-employee/:id" element={<CreateEmployeeComponent />} />
+            <Route path="/view-employee/:id" element={<ViewEmployeeComponent />} />
+            {/* <Route path="/update-employee/:id" element={<UpdateEmployeeComponent />} /> */}
+          </Routes>
+        </div>
+        <FooterComponent />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
