6799aafc2fbb583038bdd2ff4ab0ee3b94119aa8
LpMashinini <lebogangmatshinini@gmail.com>
2026-08-10 19:21:44 +0200

    add securityConfig and updated docker compose

--- Backend/src/main/java/com/example/Backend_Springboot/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/Backend_Springboot/config/SecurityConfig.java
@@ -1,20 +1,22 @@
 package com.example.Backend_Springboot.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
-                        .anyRequest().permitAll()
-                );
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/**").permitAll()
+                .anyRequest().permitAll()
+            );
 
         return http.build();
     }
