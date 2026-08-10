22f57e3317d31c291fc3db18a6ab57941c194f92
LpMashinini <lebogangmatshinini@gmail.com>
2026-08-10 17:29:19 +0200

    add security config

---
+++ Backend/src/main/java/com/example/Backend_Springboot/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.Backend_Springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
